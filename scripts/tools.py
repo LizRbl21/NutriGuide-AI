b38bff4c3f340c92a082b65918c5971e10073653
Elizabeth Robles Orozco <robleseli9@gmail.com>
2026-07-22 19:52:57 -0600

    Añadiendo funciones clave para testeo..

--- scripts/tools.py
+++ scripts/tools.py
@@ -2,3 +2,16 @@ from query_engine import NutritionEngine
 
 engine = NutritionEngine()
 
+# Buscar algun alimento
+def search(food):
+    return engine.find_food(food)
+
+# Comparar alimentos
+def compare(food_1, food_2):
+    return engine.compare_foods(
+        [food_1, food_2]
+    )
+
+# Top 10 en X nutriente
+def top_on(nutrient):
+    return engine.top_by_nutrient(nutrient)
